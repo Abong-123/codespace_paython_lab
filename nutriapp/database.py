a443c52da1aa35003d7219212ee4427480652f3c
Abrar Wira <134397409+Abong-123@users.noreply.github.com>
2026-03-06 18:53:21 +0000

    fix

--- nutriapp/database.py
+++ nutriapp/database.py
@@ -1,15 +1,7 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
+DATABASE_URL = "postgresql://appuser:password@localhost:5432/nutriapp"
 
 engine = create_engine(DATABASE_URL)
 
